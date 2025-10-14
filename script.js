cd4546cdd0454fa8d3e97913c6d55b2c8d4d7bb3
Art0citus <mritik424@gmail.com>
2025-10-14 23:25:02 +0530

    Clock project complete

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-const form = document.querySelector('Form')
+const form = document.querySelector("Form");
 
-form.addEventListener('submit', function(e){
-    e.preventDefault();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-   const height =  parseInt(document.querySelector('#height').value)
-   const weight =  parseInt(document.querySelector('#weight').value)
-   const result =  (document.querySelector('#result'))
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const result = document.querySelector("#result");
 
-   if(height === '' || height < 0 || isNaN(height)){
+  if (height === "" || height < 0 || isNaN(height)) {
     result.innerHTML = `Please put invalid height ${height}`;
-   }
+  }
 
-    if(weight === '' || weight < 0 || isNaN(weight)){
+  if (weight === "" || weight < 0 || isNaN(weight)) {
     result.innerHTML = `Please put invalid weight  ${weight}`;
-   } else {
-     const  bmi = (weight / ((height*height)/10000)).toFixed(2)
-     result.innerHTML = `<span>${bmi}</span>`;
-   }
-
-})
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    result.innerHTML = `<span>${bmi}</span>`;
+  }
+});
